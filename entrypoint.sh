edf976924734c217b9bd7327c3fe395d458660a6
Fredrik Jansson <fj@avassa.io>
2023-06-21 13:54:46 +0200

    More logging.

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,12 @@ password="$3"
 app_name="$4"
 app_spec="$5"
 
+echo "Download supctl"
 curl -s -f -OL "https://$ct/supctl"
 chmod +x ./supctl
 
+echo "Login"
 echo "$password" | ./supctl --host="$ct" "do" login "$username" > /dev/null
 
+echo "Send application spec"
 ./supctl replace applications "$app_name" < "$app_spec"
